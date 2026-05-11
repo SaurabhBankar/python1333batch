1c587af1157c693dc02cd896f4aea7fa13e6e3ac
Saurabh Bankar <161301961+SaurabhBankar@users.noreply.github.com>
2026-05-11 18:40:43 +0530

    Eval function
    using eval function we can use multiple operation like addition, subtraction, multiplication, division, & also change typecasting.

---
+++ may5.py
@@ -0,0 +1,7 @@
+expr ="20+45/5-3*2"
+res = eval(expr)
+print(res)
+
+v1 = eval(input("Enter a number: "))
+print("you entered:", v1)
+print("Type of v1: ",type(v1)) #the Type of v1 is int if you enter an integer, float and float if you enter a decimal number.
